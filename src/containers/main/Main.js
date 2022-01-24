88ff28c3c665c66607e6b15698513aa2f133b7ee
Tohir <TOXIR_DEV>
2022-01-24 22:07:08 +0600

    I'm making section movies of main.

--- src/containers/main/Main.js
+++ src/containers/main/Main.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSectionContext } from "../../components/Sections/context";
 import Movies from "../../components/Sections/Movies/Movies";
 import Searching from "../../components/Sections/searching/Searching";
 import { PrimaryMain } from "./MainStyle";
 
 const Main = () => {
+  const tabs = [
+    { name: "Streaming", id: 1 },
+    { name: "On TV", id: 2 },
+    { name: "For Rent", id: 3 },
+    { name: "In Theaters", id: 4 },
+  ];
+  const { data, fetchMovie } = useSectionContext();
+
   return (
     <PrimaryMain>
       <Searching />
-      <Movies />
+      <Movies title={"What's Popular"} tabs={tabs} />
     </PrimaryMain>
   );
 };
